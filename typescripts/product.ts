fb7145b93e9d8d0b7472201c74e074cbf4af1bfc
arcum-omni <travis.l.eiler@gmail.com>
2019-11-12 13:47:36 -0800

    Created Product class

---
+++ typescripts/product.ts
@@ -0,0 +1,25 @@
+/**
+ * Represents a single shopping cart item.
+ */
+class Product{
+    /**
+     * The name of the product
+     */
+    title:string;
+    /**
+     * The product description
+     */
+    description:string;
+    /**
+     * The product
+     */
+    price:Number;
+}
+
+//  Test Code
+/*
+    let prod = new Product();
+    prod.title = "something";
+    prod.description = "description";
+    prod.price = 4.99;
+*/
